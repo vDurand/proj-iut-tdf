c7d435049f259fe1b6c6a21cf331a03394ca04ff
Durand <vlad.zdk@gmail.com>
2014-09-30 20:28:49 +0200

    pattern change

---
+++ functions/addAnnee.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by Valentin Durand
+ * IUT Caen - DUT Informatique
+ * Date: 30/09/14
+ * Time: 15:35
+ */
--- functions/inputRules.php
+++ functions/inputRules.php
@@ -18,6 +18,54 @@ function noAccents($str, $charset='utf-8')
     return $str;
 }
 
+// http://godefroy.me/php-enlever-tous-les-accents-d-une-chaine-a192614
+function removeAccents($txt){
+    $txt = str_replace('œ', 'oe', $txt);
+    $txt = str_replace('Œ', 'Oe', $txt);
+    $txt = str_replace('æ', 'ae', $txt);
+    $txt = str_replace('Æ', 'Ae', $txt);
+    mb_regex_encoding('UTF-8');
+    $txt = mb_ereg_replace('[ÀÁÂÃÄÅĀĂǍẠẢẤẦẨẪẬẮẰẲẴẶǺĄ]', 'A', $txt);
+    $txt = mb_ereg_replace('[ãäåāăǎạảấầẩẫậắằẳẵặǻą]', 'a', $txt);
+    $txt = mb_ereg_replace('[ÇĆĈĊČ]', 'C', $txt);
+    $txt = mb_ereg_replace('[ćĉċč]', 'c', $txt);
+    $txt = mb_ereg_replace('[ÐĎĐ]', 'D', $txt);
+    $txt = mb_ereg_replace('[ďđ]', 'd', $txt);
+    $txt = mb_ereg_replace('[ÈÉÊËĒĔĖĘĚẸẺẼẾỀỂỄỆ]', 'E', $txt);
+    $txt = mb_ereg_replace('[ēĕėęěẹẻẽếềểễệ]', 'e', $txt);
+    $txt = mb_ereg_replace('[ĜĞĠĢ]', 'G', $txt);
+    $txt = mb_ereg_replace('[ĝğġģ]', 'g', $txt);
+    $txt = mb_ereg_replace('[ĤĦ]', 'H', $txt);
+    $txt = mb_ereg_replace('[ĥħ]', 'h', $txt);
+    $txt = mb_ereg_replace('[ÌÍÎÏĨĪĬĮİǏỈỊ]', 'I', $txt);
+    $txt = mb_ereg_replace('[ìíîïĩīĭįıǐỉị]', 'i', $txt);
+    $txt = str_replace('Ĵ', 'J', $txt);
+    $txt = str_replace('ĵ', 'j', $txt);
+    $txt = str_replace('Ķ', 'K', $txt);
+    $txt = str_replace('ķ', 'k', $txt);
+    $txt = mb_ereg_replace('[ĹĻĽĿŁ]', 'L', $txt);
+    $txt = mb_ereg_replace('[ĺļľŀł]', 'l', $txt);
+    $txt = mb_ereg_replace('[ÑŃŅŇ]', 'N', $txt);
+    $txt = mb_ereg_replace('[ñńņňŉ]', 'n', $txt);
+    $txt = mb_ereg_replace('[ÒÓÔÕÖØŌŎŐƠǑǾỌỎỐỒỔỖỘỚỜỞỠỢ]', 'O', $txt);
+    $txt = mb_ereg_replace('[òóõöøōŏőơǒǿọỏốồổỗộớờởỡợð]', 'o', $txt);
+    $txt = mb_ereg_replace('[ŔŖŘ]', 'R', $txt);
+    $txt = mb_ereg_replace('[ŕŗř]', 'r', $txt);
+    $txt = mb_ereg_replace('[ŚŜŞŠ]', 'S', $txt);
+    $txt = mb_ereg_replace('[śŝşš]', 's', $txt);
+    $txt = mb_ereg_replace('[ŢŤŦ]', 'T', $txt);
+    $txt = mb_ereg_replace('[ţťŧ]', 't', $txt);
+    $txt = mb_ereg_replace('[ÙÚÛÜŨŪŬŮŰŲƯǓǕǗǙǛỤỦỨỪỬỮỰ]', 'U', $txt);
+    $txt = mb_ereg_replace('[üũūŭůűųưǔǖǘǚǜụủứừửữự]', 'u', $txt);
+    $txt = mb_ereg_replace('[ŴẀẂẄ]', 'W', $txt);
+    $txt = mb_ereg_replace('[ŵẁẃẅ]', 'w', $txt);
+    $txt = mb_ereg_replace('[ÝŶŸỲỸỶỴ]', 'Y', $txt);
+    $txt = mb_ereg_replace('[ýÿŷỹỵỷỳ]', 'y', $txt);
+    $txt = mb_ereg_replace('[ŹŻŽ]', 'Z', $txt);
+    $txt = mb_ereg_replace('[źżž]', 'z', $txt);
+    return $txt;
+}
+
 // Retire les accents des premieres lettre de chaque mot
 function firstNoAccent($a)
 {
@@ -48,13 +96,13 @@ function firstNoAccent($a)
 // formatage du nom
 function formatNom($nom)
 {
-    return strtoupper(noAccents(trim(firstNoAccent($nom), " -")));
+    return strtoupper(noAccents(trim(firstNoAccent(removeAccents($nom)), " -")));
 }
 
 // formatage du prenom
 function formatPrenom($prenom)
 {
-    return trim(firstNoAccent($prenom), " -");
+    return trim(firstNoAccent(removeAccents($prenom)), " -");
 }
 //$tt = " -- - qios'sd uud- dsd-  - -";
 //echo formatPrenom($tt)."<br>";
